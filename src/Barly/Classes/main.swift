f3f449c11cf412ac0fa916dcdbebb6378bba802a
Dominic Rodemer <dominic.rodemer@me.com>
2026-03-18 09:31:36 +0100

    Wrap main.swift in MainActor.assumeIsolated to fix Swift 6 concurrency warning

--- src/Barly/Classes/main.swift
+++ src/Barly/Classes/main.swift
@@ -7,9 +7,11 @@
 
 import Cocoa
 
-let app = NSApplication.shared
-app.setActivationPolicy(.accessory)
+MainActor.assumeIsolated {
+    let app = NSApplication.shared
+    app.setActivationPolicy(.accessory)
 
-let delegate = AppDelegate()
-app.delegate = delegate
-app.run()
+    let delegate = AppDelegate()
+    app.delegate = delegate
+    app.run()
+}
